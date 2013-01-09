212846eaff22ac0a12b5928aa0a8ba713b63b9b1
Todd Wolfson <todd@twolfson.com>
2013-01-09 01:38:11 -0800

    After a little bit of setup, grunt-mocha seems to be stale. Time to debug

--- test/Builder_test.js
+++ test/Builder_test.js
@@ -1,17 +1,7 @@
-(function($) {
-  // Mocha: http://visionmedia.github.com/mocha/
-
-  module('Builder#window', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
+describe('A Builder', function () {
+  describe('defined on window', function () {
+    it('exists', function () {
+      chai.assert(window.Builder);
+    });
   });
-
-  test('exists', function() {
-    expect(1);
-    // Not a bad test to run on collection methods.
-    ok(Builder, 'Builder defined on window exists');
-  });
-
-}(jQuery));
+});
--- test/Builder_test.require.js
+++ test/Builder_test.require.js
@@ -1,44 +1,8 @@
 /*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
-/*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
-/*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
-/*
-  ======== A Handy Little QUnit Reference ========
-  http://docs.jquery.com/QUnit
-
-  Test methods:
-    expect(numAssertions)
-    stop(increment)
-    start(decrement)
-  Test assertions:
-    ok(value, [message])
-    equal(actual, expected, [message])
-    notEqual(actual, expected, [message])
-    deepEqual(actual, expected, [message])
-    notDeepEqual(actual, expected, [message])
-    strictEqual(actual, expected, [message])
-    notStrictEqual(actual, expected, [message])
-    raises(block, [expected], [message])
-*/
-
-module('Builder#requirejs', {
-  // This will run before each test in this module.
-  setup: function() {
-    this.selector = '#qunit-fixture';
-  }
-});
-
-test('exists', function() {
-  expect(1);
-
-  // Stop the test while we load jQuery.
-  stop();
-
-  // Load jQuery.
-  require(['Builder'], function (Builder) {
-    // Continue testing.
-    start();
-
-    // Assert Builder exists
-    ok(Builder, 'Builder as defined by requirejs exists');
+describe('A Builder', function () {
+  describe('defined by require.js', function () {
+    it('exists', function () {
+      chai.assert(window.Builder);
+    });
   });
 });
